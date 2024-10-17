f0699740134ca8234d294847c3268641cc45a4d0
ThanhThanh189 <65250139+ThanhThanh189@users.noreply.github.com>
2024-10-17 23:45:57 +0700

    Feature/facebook sdk (#13)
    * create login facebook function
    
    * add library

--- lib/presentation/main/main_common.dart
+++ lib/presentation/main/main_common.dart
@@ -3,12 +3,16 @@ import 'dart:async';
 import 'package:base_module_riverpod/presentation/main/app_flavor.dart';
 import 'package:base_module_riverpod/presentation/providers/app_flavor_provider.dart';
 import 'package:base_module_riverpod/presentation/providers/app_navigator_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../app.dart';
+import '../../firebase_option_dev.dart';
 
 void mainCommon(AppFlavor appFlavor) {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Future<void> startApp() async {
     final container = ProviderContainer(
       overrides: [
@@ -32,6 +36,8 @@ void mainCommon(AppFlavor appFlavor) {
       WidgetsFlutterBinding.ensureInitialized();
       startApp();
     },
-    (error, stackTrace) {},
+    (error, stackTrace) {
+      print(error);
+    },
   );
 }
